84d0dab6c84a76bb1f00f38cbffbd7d4766ccbd5
kenji <r.bergoin@openium.fr>
2023-10-26 07:50:39 +0200

    Initial Commit

---
+++ DITestApp/DITestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DITestAppApp.swift
+//  DITestApp
+//
+//  Created by Richard Bergoin on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct DITestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
